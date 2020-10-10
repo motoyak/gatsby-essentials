ca8dea253352ae390c51a90874032a886666fcfe
Motohiro Yakura <motohiro686@yahoo.co.jp>
2020-10-11 05:29:34 +0900

    footer improvement

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,6 +126,13 @@ const Component=({data})=>(
           </li>
         </ul>
       </div>
+      <div className="back">
+        <Img
+          fluid={data.pattern.childImageSharp.fluid}
+          alt=""
+          style={{height:"100%"}}
+        />
+      </div>
     </footer>
   </div>
 )
@@ -165,6 +172,13 @@ export const query = graphql`
     berry: file(relativePath: {eq: "berry.jpg"}) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    pattern: file(relativePath: {eq: "pattern.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1920, quality: 90) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
